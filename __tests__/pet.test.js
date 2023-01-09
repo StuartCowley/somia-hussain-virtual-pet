6eda543064da86b04a4e3525948ff98d80d0a079
somia <somiahussain49@gmail.com>
2023-01-09 17:02:28 +0000

    Pet getting older and unhealthier

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -23,6 +23,36 @@ describe("constructor", () => {
 
         expect(pet.age).toEqual(1);
       });
+      describe("constructor", () => {
+        it("has a initial hunger of 0", () => {
+          const pet = new Pet("Fido");
+
+          expect(pet.hunger).toEqual(0);
+        });
+        describe("increaseHunger", () => {
+          it("increase hunger by five as they grow up", () => {
+            const pet = new Pet("Fido");
+            pet.growUp();
+
+            expect(pet.hunger).toEqual(5);
+          });
+          describe("constructor", () => {
+            it("has a initial fitness of 10", () => {
+              const pet = new Pet("Fido");
+
+              expect(pet.fitness).toEqual(10);
+            });
+            describe("decrease fitness", () => {
+              it("decrease fitness by 10 as pet grows up", () => {
+                const pet = new Pet("Fido");
+                pet.growUp();
+
+                expect(pet.fitness).toEqual(7);
+              });
+            });
+          });
+        });
+      });
     });
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,15 @@
 function Pet(name) {
-    this.name = name;
-    this.age = 0;
-    this.growUp = function() {
-      this.age += 1;
-    };
-  }
+  this.name = name;
+  this.age = 0;
+  this.growUp = function () {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+  };
+  this.hunger = 0;
+  this.fitness = 10;
+}
+
+// The Pet should have an initial fitness of 10
+
 module.exports = Pet;
